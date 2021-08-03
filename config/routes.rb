23620d870f721732304af50688f68b0c3aad8298
tabinoki <k.tabinoki961027@gmail.com>
2021-08-03 22:25:11 +0900

    rails-ujs読み込み

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/edit'
+  resources :posts
   resources :users
 
   get    '/login',   to: 'sessions#new'
