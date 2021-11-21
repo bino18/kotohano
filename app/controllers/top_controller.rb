70f740a28ec310bd1925548b5c674face18b1b0c
tabinoki <k.tabinoki961027@gmail.com>
2021-11-21 22:57:30 +0900

    デザイン修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -4,4 +4,19 @@ class TopController < ApplicationController
   def top
     @posts = Post.includes(:user).order(id: "DESC").limit(10)
   end
+
+  #コントローラーを作る
+  def sunset
+    def search
+      if sunset = params[:year, :month, day, lat, :lng]
+        uri = URI.parse("https://labs.bitmeister.jp/ohakon/json/?year=#{sunset.year}&month=#{sunset.month}&day=#{sunset.day}&lat=#{sunset.lat}&lng=#{sunset.lng}")
+        response = Net::HTTP.get_response(uri)
+        result = JSON.parse(response.body)
+        if result["rise_and_set"]
+          @sunset_time = result["rise_and_set"][3]["sunset_hm"]
+        end
+      end
+    end
+  
+  end
 end
